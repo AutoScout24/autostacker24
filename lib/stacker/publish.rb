c745cc12c95b95778929633795832c111c65230e
kief <kmorris@thoughtworks.com>
2014-12-12 11:56:14 +0100

    Johannes/Kief: make sure s3 client gets created properly

--- lib/stacker/publish.rb
+++ lib/stacker/publish.rb
@@ -3,14 +3,13 @@ require 'aws-sdk'
 module Stacker
 
   def self.publish_global_stack_template(template_contents, global_template_version)
-    s3 = Aws::S3::Client.new
-    s3.put_object(bucket: 'as24.tatsu.artefacts', 
+    s3client.put_object(bucket: 'as24.tatsu.artefacts', 
         key: "global-stack-template/#{global_template_version}/global-stack.json", 
         body: template_contents)
   end
 
   def self.publish_global_ami(ami_id, global_template_version)
-    s3.put_object(
+    s3client.put_object(
       bucket: 'as24.tatsu.artefacts', 
       key: "global-stack-template/#{global_template_version}/ami.txt", 
       body: ami_id
@@ -18,13 +17,16 @@ module Stacker
   end
 
   def self.publish_global_template_version(account_name, global_template_version)
-    s3 = Aws::S3::Client.new
-    s3.put_object(
+    s3client.put_object(
       bucket: 'as24.tatsu.artefacts',
       key: "global-stack-template/current-#{account_name}.txt", 
       body: global_template_version,
       acl: "bucket-owner-full-control"
     )
+  end
+
+  def s3client
+    Aws::S3::Client.new(:region => ENV['AWS_DEFAULT_REGION'] || 'eu-west-1')
   end
 
 end
