bd25d98fe13bfe86c043a5fdf1a87f935904a33c
Christian Rodemeyer <crodemeyer@autoscout24.de>
2015-01-02 10:11:00 +0100

    added gem for installing via github

--- build_stacker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -ueo pipefail
-GEM_PATH="$(ruby -rubygems -e 'puts Gem.user_dir')/bin"
-export PATH="${GEM_PATH}:${PATH:-}"
-
-bundle install --path vendor/bundle
-rake clean build publish
