ef78ef6ba7e1465f0a5fd9565a636f5896c8ea54
Johannes Müller <jmueller@autoscout24.com>
2015-02-13 13:20:52 +0100

    fix last events getting lost during deletion

--- lib/autostacker24/stacker.rb
+++ lib/autostacker24/stacker.rb
@@ -89,8 +89,10 @@ module Stacker
     stop_time   = Time.now + timeout_in_minutes * 60
     finished    = /(CREATE_COMPLETE|UPDATE_COMPLETE|DELETE_COMPLETE|ROLLBACK_COMPLETE|ROLLBACK_FAILED|CREATE_FAILED|DELETE_FAILED)$/
     puts "waiting for #{operation} stack #{stack_name}"
+    stack_id = find_stack(stack_name)[:stack_id]
 
     while Time.now < stop_time
+      sleep(7)
       stack = find_stack(stack_name)
       status = stack ? stack.stack_status : 'DELETE_COMPLETE'
       expected_status = case operation
@@ -98,14 +100,13 @@ module Stacker
                           when :update then /UPDATE_COMPLETE$/
                           when :delete then /DELETE_COMPLETE$/
                         end
-      return true if status =~ expected_status
-      raise "#{operation} #{stack_name} failed, current status #{status}" if status =~ finished
-      new_events = get_stack_events(stack_name).select{|e| !seen_events.include?(e[:event_id])}.sort_by{|e| e[:timestamp]}
+      new_events = get_stack_events(stack_id).select{|e| !seen_events.include?(e[:event_id])}.sort_by{|e| e[:timestamp]}
       new_events.each do |e|
         seen_events << e[:event_id]
         puts "#{e[:timestamp]}\t#{e[:resource_status].ljust(20)}\t#{e[:resource_type].ljust(40)}\t#{e[:logical_resource_id].ljust(30)}\t#{e[:resource_status_reason]}"
       end
-      sleep(7)
+      return true if status =~ expected_status
+      raise "#{operation} #{stack_name} failed, current status #{status}" if status =~ finished
     end
     raise "waiting for #{operation} stack #{stack_name} timed out after #{timeout_in_minutes} minutes"
   end
@@ -153,8 +154,8 @@ module Stacker
     resources.inject({}){|map, resource| map.merge(resource.logical_resource_id.to_sym => resource)}.freeze
   end
 
-  def get_stack_events(stack_name)
-    events = cloud_formation.describe_stack_events(stack_name: stack_name).data.stack_events
+  def get_stack_events(stack_name_or_id)
+    events = cloud_formation.describe_stack_events(stack_name: stack_name_or_id).data.stack_events
   end
 
   def cloud_formation # lazy CloudFormation client
