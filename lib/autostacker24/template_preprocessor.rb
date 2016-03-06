cbd4294a624fed49894440be61e63660d6673b01
Christian Rodemeyer <christian.rodemeyer@tado.com>
2016-03-06 15:18:14 +0100

    simple one level parsing for accessing maps

--- lib/autostacker24/template_preprocessor.rb
+++ lib/autostacker24/template_preprocessor.rb
@@ -50,7 +50,8 @@ module AutoStacker24
       parts = tokenize(s).map do |token|
         case token
           when '@@' then '@'
-          when /^@/ then {'Ref' => token[1..-1]}
+          when '@[' then '['
+          when /^@/ then parse_ref(token)
           else token
         end
       end
@@ -72,8 +73,25 @@ module AutoStacker24
       end
     end
 
+    def self.parse_ref(token)
+      m = /^@([^\[]*)(\[([^,]*)(\s*,\s*(.*))?\])?$/.match(token)
+      m1 = m[1]
+      m2 = m[3]
+      m3 = m[5]
+      if m3
+        {'Fn::FindInMap' => [m1, preprocess_string(m2), m3]}
+      elsif m2
+        {'Fn::FindInMap' => [m1 + 'Map', preprocess_string('@' + m1), m2]}
+      else
+        {'Ref' => m1}
+      end
+    end
+
     def self.tokenize(s)
-      pattern = /@@|@((\w+(::)?\w+)+)/
+      # for recursive bracket matching see
+      # http://stackoverflow.com/questions/19486686/recursive-nested-matching-pairs-of-curly-braces-in-ruby-regex
+      # but for we limit ourself to one level to make things less complex
+      pattern = /@@|@\[|@(\w+(::\w+)?)(\[[^\]]*\])?/
       tokens = []
       loop do
         m = pattern.match(s)
