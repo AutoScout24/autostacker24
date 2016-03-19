afdee484fc7c5836b726885d05e624c7994ab085
Christian Rodemeyer <christian.rodemeyer@tado.com>
2016-03-19 10:00:26 +0100

    allow file substitution everywhere

--- spec/templating_spec.rb
+++ spec/templating_spec.rb
@@ -18,7 +18,8 @@ RSpec.describe 'Stacker Template Processing' do
       "content" : { "Fn::Join" : ["", ["[general]\\n", "state_file = /var/lib/awslogs/agent-state\\n", "\\n"]]},
       "single_colon": "@AWS::Stack:something@Other",
       "file_include": {
-        "UserData": "@file://./spec/example_script.sh"
+        "UserData": "@file://./spec/example_script.sh",
+        "NonUserData": "@file://./spec/example_script.sh"
       },
       "auto_encode": {
         "UserData": "auto encode"
@@ -32,64 +33,69 @@ RSpec.describe 'Stacker Template Processing' do
     EOL
   end
 
-  subject(:parsed_template) { JSON.parse(Stacker.template_body(template)) }
+  subject(:processed) { JSON.parse(Stacker.template_body(template)) }
 
   it 'generates Fn:FindInMap elements' do
     expected = { 'Fn::FindInMap' => ['EnvMap', { 'Ref' => 'Env' }, 'Key'] }
-    expect(parsed_template['find_in_map']).to eq(expected)
+    expect(processed['find_in_map']).to eq(expected)
   end
 
   it 'generates Fn:FindInMap elements by convention' do
     expected = { 'Fn::FindInMap' => ['EnvMap', { 'Ref' => 'Env' }, 'Key'] }
-    expect(parsed_template['find_in_map_convention']).to eq(expected)
+    expect(processed['find_in_map_convention']).to eq(expected)
   end
 
   it 'removes any comments into valid json' do
     begin
-      expect(parsed_template['bla']).to eq('bla')
-      expect(parsed_template['fasel']).to eq('//no comment')
+      expect(processed['bla']).to eq('bla')
+      expect(processed['fasel']).to eq('//no comment')
     rescue JSON::ParserError => e
       raise "unparsable JSON output: #{e.message}"
     end
   end
 
   it 'substitutes a single variable' do
-    expect(parsed_template['single']).to eq('Ref' => 'var1')
+    expect(processed['single']).to eq('Ref' => 'var1')
   end
 
   it 'replaces an embedded variable' do
     expected_join_hash = { 'Fn::Join' => ['', ['bla ', { 'Ref' => 'var2' }, ' ', { 'Ref' => 'bla2' }]] }
-    expect(parsed_template['embedded']).to eq(expected_join_hash)
+    expect(processed['embedded']).to eq(expected_join_hash)
   end
 
   it 'replaces variables in an array' do
-    expect(parsed_template['array']).to eq([{ 'Ref' => 'var2' }, { 'Ref' => 'var3' }])
+    expect(processed['array']).to eq([{ 'Ref' => 'var2' }, { 'Ref' => 'var3' }])
   end
 
   it 'does not replace an escaped @' do
-    expect(parsed_template['escape']).to eq('bla@bla.com')
+    expect(processed['escape']).to eq('bla@bla.com')
   end
 
   it 'preserves content' do
     expected_join_hash = { 'Fn::Join' => ['', ["[general]\n", "state_file = /var/lib/awslogs/agent-state\n", "\n"]] }
-    expect(parsed_template['content']).to eq(expected_join_hash)
+    expect(processed['content']).to eq(expected_join_hash)
   end
 
   it 'includes only double semicolon in variable name' do
     expected_join_hash = { 'Fn::Join' => ['', [{ 'Ref' => 'AWS::Stack' }, ':something', { 'Ref' => 'Other' }]] }
-    expect(parsed_template['single_colon']).to eq(expected_join_hash)
+    expect(processed['single_colon']).to eq(expected_join_hash)
   end
 
-  it 'includes a user script with newlines' do
+  it 'substitutes @file:// with file content' do
+    content = {'Fn::Join' =>['', ["#!/bin/bash\n\necho \"", {'Ref' => 'Version'}, "\"\n"]]}
+    expect(processed['file_include']['NonUserData']).to eq(content)
+  end
+
+  it 'substitutes @file:// in UserData with file content and encodes it in base64' do
     user_data = {'Fn::Join' =>['', ["#!/bin/bash\n\necho \"", {'Ref' => 'Version'}, "\"\n"]]}
-    expect(parsed_template['file_include']['UserData']).to eq('Fn::Base64' => user_data)
+    expect(processed['file_include']['UserData']).to eq('Fn::Base64' => user_data)
   end
 
   it 'wraps userData in a Base64 encoded block' do
-    expect(parsed_template['auto_encode']['UserData']).to eq('Fn::Base64' => 'auto encode')
+    expect(processed['auto_encode']['UserData']).to eq('Fn::Base64' => 'auto encode')
   end
 
   it 'does not double wrap encoded blocks' do
-    expect(parsed_template['already_encoded']['UserData']).to eq('Fn::Base64' => '#!/bin/bash')
+    expect(processed['already_encoded']['UserData']).to eq('Fn::Base64' => '#!/bin/bash')
   end
 end
