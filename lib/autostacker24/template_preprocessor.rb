8b397df8be0e0d8837dbf32539225afb40b349f6
Christian Rodemeyer <christian.rodemeyer@tado.com>
2016-03-25 19:21:06 +0100

    remove old implementation

--- lib/autostacker24/template_preprocessor.rb
+++ lib/autostacker24/template_preprocessor.rb
@@ -41,80 +41,14 @@ module AutoStacker24
       elsif json.is_a?(Array)
         json.map{|v| preprocess_json(v)}
       elsif json.is_a?(String)
-        preprocess_string(json)
+        interpolate(json)
       else
         json
       end
     end
 
     def self.preprocess_user_data(s)
-      {'Fn::Base64' => preprocess_string(s)}
-    end
-
-    def self.preprocess_string(s)
-      m = /^@file:\/\/(.*)$/.match(s)
-      s = File.read(m[1]) if m
-      parts = tokenize(s).map do |token|
-        case token
-          when '@@' then '@'
-          when '@[' then '['
-          when /\A@/ then parse_ref(token)
-          else token
-        end
-      end
-
-      # merge neighboured strings
-      parts = parts.reduce([])do |m, p|
-        if m.last.is_a?(String) && p.is_a?(String)
-          m[-1] += p
-        else
-          m << p
-        end
-        m
-      end
-
-      if parts.length == 1
-        parts.first
-      else # we need a join construct
-        {'Fn::Join' => ['', parts]}
-      end
-    end
-
-    def self.parse_ref(token)
-      m = /\A@([^\[]*)(\[([^,]*)(\s*,\s*(.*))?\])?$/.match(token)
-      m1 = m[1]
-      m2 = m[3]
-      m3 = m[5]
-      if m2
-        args = if m3
-                  [m1, m2, m3]
-               else
-                 [m1 + 'Map', '@' + m1, m2]
-               end
-        {'Fn::FindInMap' => [args[0], preprocess_string(args[1]), preprocess_string(args[2])]}
-      else
-        {'Ref' => m1}
-      end
-    end
-
-    def self.tokenize(s)
-      # for recursive bracket matching see
-      # http://stackoverflow.com/questions/19486686/recursive-nested-matching-pairs-of-curly-braces-in-ruby-regex
-      # but for we limit ourself to one level to make things less complex
-      pattern = /@@|@\[|@(\w+(::\w+)?)(\[[^\]]*\])?/
-      tokens = []
-      loop do
-        m = pattern.match(s)
-        if m
-          tokens << m.pre_match unless m.pre_match.empty?
-          tokens << m.to_s
-          s = m.post_match
-        else
-          tokens << s unless s.empty? && !tokens.empty?
-          break
-        end
-      end
-      tokens
+      {'Fn::Base64' => interpolate(s)}
     end
 
     # interpolates a string with '@' expressions and returns a string or a hash
